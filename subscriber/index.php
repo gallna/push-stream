e74240334fd432942eb8c98b88602000315bab6b
Tomasz Jonik <tom.jonik@hotmail.com>
2016-07-20 02:14:46 +0100

    Cleaned list of available outputs

--- subscriber/index.php
+++ subscriber/index.php
@@ -10,7 +10,11 @@ $app = new \Slim\App(
 );
 
 $app->get('/mpeg', function ($request, $response, $args) {
-    $content = new Stream\HttpStream("http://nginx-push-stream/sub/my_channel_1.b20");
+    $command = sprintf(
+        'ffmpeg -i %s -f mpegts pipe:',
+        'http://nginx-push-stream/sub/my_channel_1.b20'
+    );
+    $content = new Stream\Pipe\ReadPipeStream($command);
     return $response
         ->withHeader('Content-Type', 'application/octet-stream')
         ->withBody($content);
@@ -27,26 +31,15 @@ $app->get('/mp4', function ($request, $response, $args) {
         ->withBody($content);
 });
 
-$app->get('/mp4-direct', function ($request, $response, $args) {
-    $content = new Stream\HttpStream("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4");
+$app->get('/direct', function ($request, $response, $args) {
+    $content = new Stream\HttpStream("http://nginx-push-stream/sub/my_channel_1.b20");
     return $response
-        ->withHeader('Content-Type', 'video/mp4')
+        ->withHeader('Content-Type', 'application/octet-stream')
         ->withBody($content);
 });
 
-$app->get('/mp4-decoded', function ($request, $response, $args) {
-    $command = sprintf(
-        'ffmpeg -i "%s" -f mpegts pipe:',
-        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
-    );
-    $content = new Stream\Pipe\ReadPipeStream($command);
-    return $response
-        ->withHeader('Content-Type', 'video/mp4')
-        ->withBody($content);
-});
-
-$app->get('/mp4-piped', function ($request, $response, $args) {
-    $source = new Stream\HttpStream("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4");
+$app->get('/piped', function ($request, $response, $args) {
+    $source = new Stream\HttpStream("http://nginx-push-stream/sub/my_channel_1.b20");
     $command = sprintf(
         'ffmpeg -i pipe: -f mpegts pipe:'
     );
