76673ad1507942302a5216369c005dda854af8c9
Tomasz Jonik <tom.jonik@hotmail.com>
2016-07-31 21:19:52 +0100

    Added multi-channel support

--- publisher/index.php
+++ publisher/index.php
@@ -5,74 +5,116 @@ use Slim\Http\Response;
 
 include_once 'vendor/autoload.php';
 
+$publisher = $id = null;
+
+use Psr\Http\Message\StreamInterface;
+class Publisher
+{
+    private $id;
+    private $client;
+    private $stream;
+
+    public function __construct($id, StreamInterface $stream = null)
+    {
+        $this->id = $id;
+        $stream and $this->setStream($stream);
+    }
+
+    public function setStream(StreamInterface $stream)
+    {
+        $this->stream = $stream;
+    }
+
+    public function push(GuzzleHttp\Client $client)
+    {
+        $this->client = $client;
+        $this->pushStream($this->stream);
+    }
+
+    public function delete()
+    {
+        $this->client->request(
+            'DELETE',
+            "/pub?id={$this->id}"
+        );
+        error_log("channel deleted");
+    }
+
+    public function pushStream(StreamInterface $stream)
+    {
+        while (!$stream->eof()) {
+            try {
+                if (!($body = $stream->read(10000))) {
+                    error_log("no more body");
+                    break;
+                }
+            } catch (\Exception $e) {
+                error_log(sprintf(
+                    "stream exception %s - %s",
+                    $e->getCode(),
+                    $e->getMessage()
+                ));
+                break;
+            }
+
+            $response = $this->client->request(
+                'POST',
+                "/pub?id={$this->id}",
+                ['body' => $body]
+            );
+            if ($response->getStatusCode() != 200) {
+                $this->delete();
+                error_log(sprintf(
+                    "nginx-push-stream server response %s - %s",
+                    $response->getStatusCode(),
+                    $response->getReasonPhrase()
+                ));
+            }
+        }
+        error_log("stream finished");
+        $this->delete();
+    }
+}
+
+
+
 $app = new \Slim\App(
     $container = new \Slim\Container(include 'config/container.config.php')
 );
 
-$app->get('/mp4', function ($request, $response, $args) use ($app) {
+$app->post('/mp4', function ($request, $response, $args) use ($app, &$publisher, &$id) {
     if (false === ($uri = $request->getParam("uri", false))) {
         return $response->withStatus(400);
     }
+    if (false === ($id = $request->getParam("id", false))) {
+        return $response->withStatus(400);
+    }
     try {
-        $content = new Stream\HttpStream($uri);
+        $stream = new Stream\HttpStream($uri);
     } catch (\InvalidArgumentException $e) {
         return $response->withStatus(400);
     }
 
-    $client = new GuzzleHttp\Client(['base_uri' => 'http://nginx-push-stream']);
-
-    $app->respond($response->withStatus(201));
-    fastcgi_finish_request();
-
-    while (!$content->eof()) {
-        if (!($body = $content->read(10000))) {
-            continue;
-        }
-        $response = $client->request(
-            'POST',
-            '/pub?id=my_channel_1',
-            ['body' => $body]
-        );
-        if ($response->getStatusCode() != 200) {
-            error_log(sprintf(
-                "nginx-push-stream server response %s - %s",
-                $response->getStatusCode(),
-                $response->getReasonPhrase()
-            ));
-            die;
-        }
-    }
+    $publisher = new Publisher($id, $stream);
+    return $response;
 });
 
-$app->get('/mpeg', function ($request, $response, $args) use ($app) {
+$app->post('/mpeg', function ($request, $response, $args) use ($app, &$publisher, &$id) {
     if (false === ($uri = $request->getParam("uri", false))) {
         return $response->withStatus(400);
     }
-    $command = sprintf('ffmpeg -re -i %s -f mpegts pipe:', $uri);
-    $content = new Stream\Pipe\ReadPipeStream($command);
-    $client = new GuzzleHttp\Client(['base_uri' => 'http://nginx-push-stream']);
-
-    $app->respond($response->withStatus(201));
-    fastcgi_finish_request();
-
-    while (!$content->eof()) {
-        if (!($body = $content->read(10000))) {
-            continue;
-        }
-        $response = $client->request(
-            'POST',
-            '/pub?id=my_channel_1',
-            ['body' => $body]
-        );
-        if ($response->getStatusCode() != 200) {
-            error_log(sprintf(
-                "nginx-push-stream server response %s - %s",
-                $response->getStatusCode(),
-                $response->getReasonPhrase()
-            ));
-            die;
-        }
+    if (false === ($id = $request->getParam("id", false))) {
+        return $response->withStatus(400);
     }
+    $command = sprintf('ffmpeg -re -i %s -f mpegts pipe:', $uri);
+    $stream = new Stream\Pipe\ReadPipeStream($command);
+    $publisher = new Publisher($id, $stream);
+    return $response;
 });
 
 $app->run();
+
+$client = new GuzzleHttp\Client(['base_uri' => 'http://nginx-push-stream']);
+
+fastcgi_finish_request();
+$publisher->push($client);
--- subscriber/index.php
+++ subscriber/index.php
@@ -2,6 +2,7 @@
 use Kemer\Stream;
 use Slim\Http\Request;
 use Slim\Http\Response;
+use GuzzleHttp\Exception as GuzzleException;
 
 include_once 'vendor/autoload.php';
 
@@ -10,9 +11,17 @@ $app = new \Slim\App(
 );
 
 $app->get('/mpeg', function ($request, $response, $args) {
+    if (false === ($id = $request->getParam("id", false))) {
+        return $response->withStatus(400);
+    }
+    try {
+        (new \GuzzleHttp\Client())->request("GET", "http://nginx-push-stream/stats?id={$id}");
+    } catch (GuzzleException\ClientException $e) {
+        return $response->withStatus($e->getCode());
+    }
     $command = sprintf(
         'ffmpeg -i %s -f mpegts pipe:',
-        'http://nginx-push-stream/sub/my_channel_1.b20'
+        "http://nginx-push-stream/sub/{$id}"
     );
     $content = new Stream\Pipe\ReadPipeStream($command);
     return $response
@@ -21,9 +30,17 @@ $app->get('/mpeg', function ($request, $response, $args) {
 });
 
 $app->get('/mp4', function ($request, $response, $args) {
+    if (false === ($id = $request->getParam("id", false))) {
+        return $response->withStatus(400);
+    }
+    try {
+        (new \GuzzleHttp\Client())->request("GET", "http://nginx-push-stream/stats?id={$id}");
+    } catch (GuzzleException\ClientException $e) {
+        return $response->withStatus($e->getCode());
+    }
     $command = sprintf(
         'ffmpeg -f mpegts -i %s -c copy -bsf:v h264_mp4toannexb -movflags empty_moov+frag_keyframe -bsf dump_extra -f mp4 pipe:',
-        'http://nginx-push-stream/sub/my_channel_1.b20'
+        "http://nginx-push-stream/sub/{$id}"
     );
     $content = new Stream\Pipe\ReadPipeStream($command);
     return $response
@@ -32,14 +49,20 @@ $app->get('/mp4', function ($request, $response, $args) {
 });
 
 $app->get('/direct', function ($request, $response, $args) {
-    $content = new Stream\HttpStream("http://nginx-push-stream/sub/my_channel_1.b20");
+    if (false === ($id = $request->getParam("id", false))) {
+        return $response->withStatus(400);
+    }
+    $content = new Stream\HttpStream("http://nginx-push-stream/sub/{$id}");
     return $response
         ->withHeader('Content-Type', 'application/octet-stream')
         ->withBody($content);
 });
 
 $app->get('/piped', function ($request, $response, $args) {
-    $source = new Stream\HttpStream("http://nginx-push-stream/sub/my_channel_1.b20");
+    if (false === ($id = $request->getParam("id", false))) {
+        return $response->withStatus(400);
+    }
+    $source = new Stream\HttpStream("http://nginx-push-stream/sub/{$id}");
     $command = sprintf(
         'ffmpeg -i pipe: -f mpegts pipe:'
     );
